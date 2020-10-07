59e7c95aac7d1166e31365edf3c3f5499c0bdce9
Roman Cattaneo <romanc@users.noreply.github.com>
2020-10-08 00:31:45 +0200

    MVP - set you zip code & query collection dates

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,12 @@
 # Züri Trash Bot
 
+Züri Trash Bot is a convenient way to query paper and cardboard collection dates for people living within Zurich.
+
+## Setup
+
+* clone the git repository
+* create a virtual environment
+* install the requirement from `requirement.txt` into your virtual environment
+* copy `config.ini.example` to `config.ini` and configure your telegram bot token
+* run `trashbot.py` from the `src` folder
 
--- src/trashbot.py
+++ src/trashbot.py
@@ -3,11 +3,12 @@
 import configparser
 import json
 import logging
+import re
 import urllib.request
 
 from datetime import datetime, date
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CallbackQueryHandler, CommandHandler, Filters, MessageHandler, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, Filters, MessageHandler, PicklePersistence, Updater
 
 # configure logger
 logging.basicConfig(
@@ -16,17 +17,44 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+ZIPCODE, CONFUSED = range(2)
+
+
 def start(update, context):
     """Handles /start command"""
-    update.message.reply_text("Hi & welcome!")
+    update.message.reply_text(
+        "Waste collection in Zurich occurs on different days depending on you zip code.\n\nPlease share your 4 digit zip code with me.")
+
+    return ZIPCODE
+
+
+def zip_handler(update, context):
+    """Tries to filter a zip code from the reply text"""
+    zips = re.findall(r"\b\d{4}\b", update.message.text)
+
+    if len(zips) == 1:
+        zip_code = zips[0]
+        context.user_data['zip_code'] = zip_code
+        update.message.reply_text(
+            "Thank you! Setting your zip code to %s" % zip_code)
+        return ConversationHandler.END
+
+    update.message.reply_text(
+        "I'm sorry, I didn't get that \U0001F61E\nPlease either share your 4 digit zip code or write /cancel to abort the conversation.")
+    return ZIPCODE
 
 
 def help(update, context):
     """Handles /help command"""
-    update.message.reply_text("You are confused? Me too! Sorry :/")
+    update.message.reply_text("You are confused? Me too! Sorry \U0001F615")
 
 
 def upcomming(update, context):
+    if 'zip_code' not in context.user_data:
+        update.message.reply_text(
+            "Hang on!\n\nI need to know where you live before I can look up the calendar for you. Please use /start first to configure this.")
+        return
+
     keyboard = [[InlineKeyboardButton("\U0001F4F0", callback_data='paper'),
                  InlineKeyboardButton("\U0001F4E6", callback_data='cardboard'),
                  InlineKeyboardButton("\U0001F455", callback_data='textile')],
@@ -57,14 +85,19 @@ def button(update, context):
 
     baseurl = "http://openerz.metaodi.ch/api/calendar"
     openerz = "%s/%s.json?zip=%s&start=%s&offset=0&limit=1" % (
-        baseurl, query.data, "8006", datetime.today().strftime('%Y-%m-%d'))
+        baseurl, query.data, context.user_data['zip_code'], datetime.today().strftime('%Y-%m-%d'))
 
     with urllib.request.urlopen(openerz) as url:
         data = json.loads(url.read().decode())
-        next_date = date.fromisoformat(
-            data['result'][0]['date']).strftime('%a %x')
+        if data['_metadata']['total_count'] == 0:
+            query.edit_message_text("I couldn't find any %s in your area \U0001F62D (zip code = %s).\n\nIf you think your zip code is wrong, use /start to configure a new one." % (
+                name[query.data], context.user_data['zip_code']))
+            return
 
-    query.edit_message_text(text="Next %s in your area: %s" %
+        next_date = date.fromisoformat(
+            data['result'][0]['date']).strftime('%a, %b %d %Y')
+
+    query.edit_message_text(text="Next %s in your area:\n%s" %
                             (name[query.data], next_date))
 
 
@@ -73,14 +106,45 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
+def cancel(update, context):
+    update.message.reply_text("Okay. Gotta go, bye! \U0001F44B")
+    return ConversationHandler.END
+
+
+def settings(update, context):
+    zip = context.user_data.get('zip_code', 'undefined')
+    update.message.reply_text(
+        "I store only very few settings. Currently, I know the following about you:\n\n\t\U00002022 zip code: %s\n\nIf you want to change your zip-code, use /start to configure a new one.\n\nUse /clear to remove all user data mentioned above." % zip)
+
+
+def clear(update, context):
+    context.user_data.clear()
+    update.message.reply_text(
+        "All user data cleared!\n\nYou will need to user /start again to configure your zip code.")
+
+
 def trashbot(token):
     """Entry point for trashbot"""
-    updater = Updater(token, use_context=True)
+    pickle = PicklePersistence(filename="trashbot_data")
+    updater = Updater(token, persistence=pickle, use_context=True)
 
     myDispatcher = updater.dispatcher
 
-    myDispatcher.add_handler(CommandHandler("start", start))
+    start_conv = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            ZIPCODE: [MessageHandler(Filters.text & ~Filters.command, zip_handler)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    myDispatcher.add_handler(start_conv)
     myDispatcher.add_handler(CommandHandler("help", help))
+    myDispatcher.add_handler(CommandHandler("cancel", cancel))
+    myDispatcher.add_handler(CommandHandler("settings", settings))
+    myDispatcher.add_handler(CommandHandler("clear", clear))
 
     myDispatcher.add_handler(MessageHandler(
         Filters.text & ~Filters.command, echo))
@@ -94,8 +158,8 @@ def trashbot(token):
 
 
 if __name__ == '__main__':
-    print("Parsing configfile")
+    logger.info("Parsing configfile")
     config = configparser.ConfigParser()
     config.read("../config.ini")
-    print("Running Züri Trash Bot")
+    logger.info("Running Züri Trash Bot")
     trashbot(config['api.telegram.org']['token'])
